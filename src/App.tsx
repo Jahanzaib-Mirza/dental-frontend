02d680cdb8bba997aeb57bda663622e353811a73
Muhammad Jahanzaib <jahanzaibm29@gmail.com>
2025-05-24 02:37:46 +0500

    redirect to login page issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,30 @@ import { Provider } from 'react-redux';
 import { store } from './lib/store/store';
 import { router } from './routes';
 import { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
+import { getProfile } from './lib/store/slices/authSlice';
+import { useAppDispatch } from './lib/hooks';
+
+// New component to house the logic that depends on Redux store
+function AppContent() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getProfile());
+  }, [dispatch]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster position="bottom-right" />
+    </>
+  );
+}
 
 function App() {
   return (
     <Provider store={store}>
-      <RouterProvider router={router} />
-      <Toaster position="bottom-right" />
+      <AppContent />
     </Provider>
   );
 }
